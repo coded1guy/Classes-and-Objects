e5ecaaf77888eb18087e1ba1430fcce9e7529b59
Emmanuel Olubiyi <42422184+coded1guy@users.noreply.github.com>
2022-05-28 23:51:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 class Student:
-    # [assignment] Skeleton class. Add your code here
-    def __init__(self):
-        pass
-
-
+    name = ""
+    age = ""
+    tracks = ""
+    score = ""
+    def __init__(self, name, age, tracks, score):
+        self.name = name
+        self.age = age
+        self.tracks = tracks
+        self.score = score    
+    def change_name(self, name):
+        self.name = name
+    def change_age(self, age):
+        self.age = age
+    def add_track(self, track):
+        self.tracks.append(track)
+    def get_score(self):
+        return self.score
 
 Bob = Student(name="Bob", age=26, tracks=["FE","BE"],score=20.90)
-
+print(Bob.name, Bob.age, Bob.tracks, Bob.score)
 # Expected methods
 Bob.change_name("Peter")
 Bob.change_age(34)
 Bob.add_track("UI/UX")
 Bob.get_score()
+print(Bob.name, Bob.age, Bob.tracks, Bob.get_score())
